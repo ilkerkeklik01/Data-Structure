b1461c325b954033bd6b0dc5a4f6e394121c6a74
Ilker Keklik <ilkerkeklik50@gmail.com>
2022-11-11 23:39:19 +0300

    11.11.22

---
+++ LinkedListImplementationOfQueues.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node{
+	int data;	
+	struct node* next;
+	
+}node;
+
+typedef node* nodePtr;
+
+
+
+//Linked list implementation of queue
+//FIFO
+//dequeue from the beginning of the linked list
+//enqueue to the last of the linked list
+
+void enqueue(nodePtr* root,int x,nodePtr* end);
+
+int dequeue(nodePtr* root);
+
+void bastir(node* r);
+int main(){
+	nodePtr root = NULL;
+	nodePtr end = NULL;
+	
+	enqueue(&root,1,&end);
+	enqueue(&root,2,&end);
+	enqueue(&root,3,&end);
+	enqueue(&root,4,&end);
+	enqueue(&root,5,&end);
+	enqueue(&root,6,&end);
+	enqueue(&root,7,&end);
+	enqueue(&root,8,&end);
+	enqueue(&root,9,&end);
+	
+	dequeue(&root);
+	dequeue(&root);
+	dequeue(&root);
+	dequeue(&root);
+	
+	
+	enqueue(&root,6,&end);
+	enqueue(&root,7,&end);
+	enqueue(&root,8,&end);
+	enqueue(&root,9,&end);
+	
+	dequeue(&root);
+	dequeue(&root);
+	dequeue(&root);
+	dequeue(&root);
+	
+	bastir(root);
+	
+	
+	return 0;
+}
+
+
+void enqueue(nodePtr* root,int x,nodePtr* end){
+		if(*root==NULL){
+			*root= (nodePtr)malloc(sizeof(node));
+			(*root)->data=x;
+			(*root)->next=NULL;
+			*end = *root; 	 
+			
+			return ;
+		}
+		
+		(*end)->next = (nodePtr)malloc(sizeof(node));
+		(*end)->next->data=x;
+		(*end)->next->next=NULL;
+		*end = (*end)->next;
+	
+}
+
+int dequeue(nodePtr* root){
+	if(root==NULL){
+		printf("Sira bos");
+		return -1;
+	}
+	
+	int rValue =(*root)->data;
+	node* temp = *root;
+	*root=(*root)->next;
+	free(temp);
+	return rValue;
+	
+	
+}
+void bastir(node* r){
+	
+	
+	
+	while(r!=NULL){
+		
+		printf("%d ",r->data);
+		r=r->next;
+	}
+	
+}
+
+
+
